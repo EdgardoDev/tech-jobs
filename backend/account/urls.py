f6af42bf39cb1213106da086a678c73af217dec5
EdgardoDev <ep-marketing-media@live.com>
2022-03-06 15:51:08 +0100

    New functionality to update user profile

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('current-user/', views.currentUser, name='current_user'),
+    path('current-user/update', views.updateUser, name='update_user'),
 ]
