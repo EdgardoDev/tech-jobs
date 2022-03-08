8e5e507f56d7d66550403f8df9c848fffdbec017
EdgardoDev <ep-marketing-media@live.com>
2022-03-08 14:25:12 +0100

    Added styles and created first components

---
+++ frontend/components/layout/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-1">
+      <p className="text-center mt-1">
+        Tech Jobs - 2022, All Rights Reserved
+        <a
+          className="ml-4"
+          rel="noreferrer"
+          target="_blank"
+          href=""
+        >
+          
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/components/layout/Header.js
@@ -0,0 +1,36 @@
+import React from "react"
+import Link from "next/link"
+import Image from "next/image"
+
+const Header = () => {
+  return (
+    <div className="nav-wrapper">
+      <div className="nav-container">
+        <Link href="/">
+          <div className="logo-wrapper">
+            <div className="logo-image-wrapper">
+              <Image width="75" height="75" src="/images/logo-header.png" alt="" />
+            </div>
+            <span className="logo-one">TECH</span>
+            <span className="logo-two">JOBS</span>
+          </div>
+        </Link>
+        <div className="buttons-wrapper">
+          <Link href="/employeer/jobs/new">
+            <button className="post-a-job-button">
+              <span><i aria-hidden className="fas fa-briefcase"></i> POST JOB</span>
+            </button>
+          </Link>
+
+          <Link href="/login">
+            <button className="login-button-header">
+              <span><i aria-hidden className="fas fa-arrow-right"></i> LOGIN</span>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
